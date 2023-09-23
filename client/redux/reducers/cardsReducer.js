7210057a345f5092e129b5abde3971646ff7de5b
tbensalem <tbensalem@yahoo.com>
2023-09-23 15:05:53 -0400

    small change to data type accepted in reducer

--- client/redux/reducers/cardsReducer.js
+++ client/redux/reducers/cardsReducer.js
@@ -2,17 +2,19 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const cardSlice = createSlice({
     name: 'cards',
-    initialState: {id: null, words: null, definition: null},
+    initialState: {id: null, word: null, definition: null, user_id: null},
     reducers: {
         createCard(state, action) {
             state[id] = payload.id,
-            state[words] = payload.words,
+            state[word] = payload.word,
             state[definition] = payload.definition
+            state[user_id] = payload.user_id
         },
         editCard(state, action) {
             state[id] = payload.id,
-            state[words] = payload.words,
+            state[word] = payload.word,
             state[definition] = payload.definition
+            state[user_id] = payload.user_id
         },
         deleteCard(state, action) {
             state[id] = payload.id
