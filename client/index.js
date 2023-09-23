ebc0560abfffdebeab864fb48002495d57c31253
tbensalem <tbensalem@yahoo.com>
2023-09-23 12:09:29 -0400

    deleted extra file

--- client/index.js
+++ client/index.js
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from './App.jsx';
 import './styles.scss';
-import store from "./store.js";
+import store from "./redux/store.js";
+
 
 const root = createRoot(document.getElementById("root"));
 root.render(
