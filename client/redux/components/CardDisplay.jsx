e588360b7941ad8e3cd53c653181f854a2fceb0c
tbensalem <tbensalem@yahoo.com>
2023-09-23 14:57:28 -0400

    cardCreator finished

--- client/redux/components/CardDisplay.jsx
+++ client/redux/components/CardDisplay.jsx
@@ -1,1 +1,24 @@
 // Presentational component
+import React from "react";
+import Card from './Card.jsx'
+
+const CardDisplay = props => {
+
+
+
+const allCards = [];
+
+// THIS WILL MAKE A FETCH REQUEST
+// WILL RECIEVE ALL OF THE CARDS
+// WILL ITERATE OVER THEM AND PUSH <DIVS> THAT HAVE A CARD IN IT TO ALL CARDS ARRAY
+// EACH CARD WILL HAVE AN ID, WORD, DEFINITION
+
+    return (
+        <div id="PUT A REAL ID HERE">
+            <h2>All Cards</h2>
+            {allCards}
+        </div>
+    )
+}
+
+export default CardDisplay;
