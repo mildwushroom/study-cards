c9b5c13f95e3427c02f00a8e09d4e8ecf3df2217
James Huang <jhuang4647@gmail.com>
2023-09-25 15:11:59 -0400

    post request works

--- client/redux/reducers/cardsReducer.js
+++ client/redux/reducers/cardsReducer.js
@@ -1,21 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const cardSlice = createSlice({
-    name: 'cards',
-    initialState: {id: null, word: null, definition: null, user_id: null},
-    reducers: {
-        createCard(state, action) {
-            const { payload } = action;
-            console.log('ACTION', action)
-            // const { id, word, definition, user_id } = payload;
+const initialState = {
+    entities: [],
+    loading: false,
+}
 
-            // state.id = payload.id,
-            // state.word = payload.word,
-            // state.definition = payload.definition,
-            // state.user_id = payload.user_id
+// fetch('/api/cards', header)
+// .then((data) => data.json())
+// .then((data) => console.log('DATA', data))
+// .catch((err) => console.error(err))
 
-            console.log('STATE FROM REDUCER', state)
+// let defaultHeader = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify(card)
+// }
+
+export const postCardThunk = createAsyncThunk('cards/createCard', async (card, thunkAPI) =>{
+    let defaultHeader = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
         },
+        body: JSON.stringify(card)
+      }
+    const parsedData = await fetch('/api/cards', defaultHeader).then((data) => data.json())
+    console.log('PARSED DATA', parsedData)
+    return parsedData;
+});
+
+export const cardSlice = createSlice({
+    name: 'cards',
+    initialState,
+    reducers: {
+        // createCard(state, action) {
+        //     const { payload } = action;
+        //     console.log('ACTION', action)
+        //     // const { id, word, definition, user_id } = payload;
+        //     // state.id = payload.id,
+        //     // state.word = payload.word,
+        //     // state.definition = payload.definition,
+        //     // state.user_id = payload.user_id
+        //     // state.entities = payload;
+
+        //     console.log('STATE FROM REDUCER', state)
+        // },
         // editCard(state, action) {
         //     const { payload } = action;
 
@@ -30,15 +61,28 @@ const cardSlice = createSlice({
         //     state[id] = payload.id
         // },
     },
+    extraReducers: {
+        [postCardThunk.pending]: (state) => {
+          state.loading = true
+        },
+        [postCardThunk.fulfilled]: (state, { payload }) => {
+            state.loading = false
+            state.entities = payload;
+            console.log('PROMISE WAS FULFILLED')
+            console.log('STATE', state.entities)
+        },
+        [postCardThunk.rejected]: (state) => {
+          state.loading = false
+        },
+      },
 })
 
-console.log(cardSlice)
+// console.log(cardSlice)
 
 const { actions, reducer } = cardSlice;
 const { createCard, editCard, deleteCard} = actions
 
-export default cardSlice;
-
+// export default cardSlice.reducer
 
 
 
