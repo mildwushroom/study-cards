099319ddb213b1fe5a817d2249cba4d11031ce30
thalesIII <tommyhalesiii@gmail.com>
2023-09-27 15:35:19 -0400

    added QuizPage

---
+++ client/pages/QuizPage.js
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom'
+import React from "react";
+
+const QuizPage = (props) => {
+    const category = useParams();
+
+    return(
+        <p> Quiz page: {category} </p>
+    )
+
+}
+
+export default QuizPage;
